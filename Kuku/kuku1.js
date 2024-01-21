fd9ccee6cef3c0966e329988c8aa81965c56c41e
mt02v <141305346+mt02v@users.noreply.github.com>
2024-01-21 13:38:44 +0000

    [Finish] 九九の表

--- Kuku/kuku1.js
+++ Kuku/kuku1.js
@@ -1,8 +1,12 @@
-let html = "";
-for(let y = 1; y <= 99; y += 1){
+let html = '<tr><th></th>'
+for(let x = 1; x <= 99; x += 1){
+  html += '<th>' + x + '</th>';
+}
   html += '<tr>';
+for(let y = 1; y <= 99; y += 1){
+  html += '<tr><th>' + y + '</th>';
   for(let x = 1; x <= 99; x += 1){
-    html += '<td>' + x * y + '</td>';
+  html += '<td>' + x * y + '</td>';
   }
   html += '</tr>';
 }
