6d4c2537c714c3b4a5ff584ba28adfef68ea0fd3
mt02v <141305346+mt02v@users.noreply.github.com>
2024-01-22 07:43:36 +0000

    [Finish] 04 カーソルキーで移動する

--- AdvJS/advmap.js
+++ AdvJS/advmap.js
@@ -4,3 +4,28 @@ let showMap = () =>{
   elem.innerHTML = '<img src="img/' + images[chip] + '.png"><img src="img/boy.png">';
 }  ;
 showMap();
+showMap();
+
+document.addEventListener('keyup',(event) => {
+  let keyname = event.key;
+  console.log(keyname);
+  let fname = images[map[x][y]];
+  if(keyname == 'ArrowLeft'){
+   if(fname.index0f('left') > -1){
+    x -= 1;
+  }
+  } else if(keyname == 'ArrowRight'){
+   if(fname.index0f('right') > -1){  
+    x += 1;
+  }
+  } else if(keyname == 'ArrowUp'){
+   if(fname.index0f('up') > -1){
+    y -= 1;
+  }
+  } else if(keyname == 'ArrowDown'){
+   if(fname.index0f('down') > -1){
+    y += 1;
+  }
+}
+  showMap()
+}  );
